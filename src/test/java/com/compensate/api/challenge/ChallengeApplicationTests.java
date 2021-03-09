f8119af0843f94da0a3804e4d7e5501409662a1b
Viktoriia Kuznietsova <viktoriia.kuznietsova@sok.fi>
2021-03-09 20:23:49 +0200

    Init project

---
+++ src/test/java/com/compensate/api/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compensate.api.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
